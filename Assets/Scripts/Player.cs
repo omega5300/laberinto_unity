9c78bacd50f41198730f3e3a8fd8f079139035b9
PVDLABSCALI <PBDLABSCALI@iMac-de-PVDLABSCALI.local>
2019-06-13 11:39:22 -0500

    unity laberinto

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -11,20 +12,39 @@ public class Player : MonoBehaviour
     private AudioSource AudioMaster;
     public AudioClip AudioCoin;
 
+    public Text txtCoins;
+    public float Coins;
+    public Text txtTime;
+    private float TimeValue;
+    private Scene EscenaActual;
+
     // Start is called before the first frame update
     void Start()
     {
-        AudioMaster = this.GetComponent<AudioSource> ();
+        TimeValue = 30f;
+        EscenaActual = SceneManager.GetActiveScene();
+        AudioMaster = this.GetComponent<AudioSource>();
         Rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float H= Input.GetAxis("Horizontal");
-        float V= Input.GetAxis("Vertical");
+        TimeValue = TimeValue - Time.deltaTime;
+
+        if (TimeValue <= 0)
+        {
+            TimeValue = 0.7f;
+            SceneManager.LoadScene(EscenaActual.name);
+        }
+
+        txtTime.text = "Tiempo: "+TimeValue.ToString ("F0");
+
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
         mivector = new Vector3(H, 0, V);
         Rb.AddForce(mivector * Fuerza * Time.deltaTime);
+
         if (Input.GetKey(KeyCode.Escape))
         {
             Application.Quit();
@@ -35,25 +55,33 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.tag == "Coin")
         {
+            Coins = Coins+1;
+            TimeValue = TimeValue + 3f;
+            txtCoins.text = "Score: "+Coins.ToString("F0");
             AudioMaster.PlayOneShot(AudioCoin);
             other.gameObject.SetActive(false);
         }
 
         if (other.gameObject.tag == "Finish")
         {
-            /*
-             * la escena se pone tanto el valor numerico o el nombre de la escena
-             * ejemplos:
-             * 
-             * SceneManager.LoadScene(1);
-             * 
-             * o           
-             * 
-             * SceneManager.LoadScene("lab2");           
-             * 
-             */
-
             SceneManager.LoadScene(1);
+        }
+
+        if (other.gameObject.tag == "Finish")
+        {
+            if (EscenaActual.name == "Lab1")
+            {
+                SceneManager.LoadScene("Lab2");
+            }
+            else
+            {
+                SceneManager.LoadScene("Lab1");
+            }
+        }
+
+        if (other.gameObject.tag == "Damage")
+        {
+            SceneManager.LoadScene (EscenaActual.name);
         }
 
     }
